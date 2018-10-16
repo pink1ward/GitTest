67d6b0182f60c62926358d2e87496af96fe60ab7
gaohan <1564813150@qq.com>
2018-10-16 16:20:01 +0800

    maven+git 测试

---
+++ GitDemo/GitTest/GitDemo/src/test/FirstCommitFile.java
@@ -0,0 +1,12 @@
+package test;
+
+public class FirstCommitFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("这是在dev分支上进行开发");
+		System.out.println("这是一个来自本地的修改");
+		System.out.println("这是一个来自远程仓库的修改");
+	}
+
+}
