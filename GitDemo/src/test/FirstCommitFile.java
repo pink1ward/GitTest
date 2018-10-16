023acd158862615e777bb29917ff51884bc35dc8
gaohan <1564813150@qq.com>
2018-10-16 13:52:20 +0800

    测试远程

--- GitDemo/src/test/FirstCommitFile.java
+++ GitDemo/src/test/FirstCommitFile.java
@@ -4,7 +4,8 @@ public class FirstCommitFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("这是在dev分支上进行开发");
+		System.out.println("这是一个来自本地的修改");
 	}
 
 }
