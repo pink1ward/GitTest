8a463748f7868c89e01f6a5b049f6dd332aaf4ae
gaohan <1564813150@qq.com>
2018-10-16 14:22:01 +0800

    test

--- GitDemo/src/test/FirstCommitFile.java
+++ GitDemo/src/test/FirstCommitFile.java
@@ -4,9 +4,12 @@ public class FirstCommitFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("这是在dev分支上进行开发");
+		System.out.println("这是在dev分支上进行第二次开发");
 		System.out.println("这是一个来自本地的修改");
 		System.out.println("这是一个来自远程仓库的修改");
+		System.out.println("dev分支");
+		System.out.println("dev分支手打多多所所所所所所所");
+		System.out.println("dsadsd");
 	}
 
 }
